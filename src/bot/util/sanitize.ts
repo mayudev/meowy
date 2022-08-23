3256c734022947bca5fe5aa5dc2a2913fc55c81b
mayudev <99561108+mayudev@users.noreply.github.com>
2022-08-23 16:24:51 +0200

    feat: implement string sanitization

--- src/bot/util/sanitize.ts
+++ src/bot/util/sanitize.ts
@@ -1,4 +1,14 @@
-// TODO
+const escaped = ['*', '#', '>', '|', '-', '+', '_', '!!', '~', '`', '^', '$'];
+
+/**
+ * Sanitize a string of markdown and mentions
+ */
 export default function sanitize(content: string) {
+  // TODO ping sanitization
+  // Markdown sanitization
+  for (const char of escaped) {
+    content = content.replace(new RegExp(`(\\\\*)\\${char}`, 'g'), `\\${char}`);
+  }
+
   return content;
 }
