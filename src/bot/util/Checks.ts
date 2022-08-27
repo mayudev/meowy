ab2acb5e0e352f6cedece1103d0d027a3dc5ecbf
mayudev <99561108+mayudev@users.noreply.github.com>
2022-08-27 13:08:18 +0200

    refactor: split permission checks

--- src/bot/util/Checks.ts
+++ src/bot/util/Checks.ts
@@ -9,11 +9,49 @@ const rawMessages = (intent: string) => ({
 
 type AllowedActions = 'BanMembers' | 'KickMembers';
 
+enum Target {
+  Neither,
+  Bot,
+  Author,
+}
+
 function hasPermission(member: Member, server: Server, action: AllowedActions) {
   return member.hasPermission(server, action);
 }
 
 export default class Checks {
+  /**
+   * Checks if the bot has enough permissions to perform the action
+   */
+  static botHasPermission(message: Message, action: AllowedActions) {
+    return hasPermission(
+      message.channel!.server!.member!,
+      message.channel!.server!,
+      action
+    );
+  }
+
+  /**
+   * Checks if the invoking user has enough permissions to perform the action
+   */
+  static userHasPermission(message: Message, action: AllowedActions) {
+    return hasPermission(message.member!, message.channel!.server!, action);
+  }
+
+  /**
+   * Checks if the target is the bot or the user themselves
+   */
+  static checkTarget(message: Message, target: Member) {
+    switch (target._id.user) {
+      case message.author_id:
+        return Target.Author;
+      case message.client.user?._id:
+        return Target.Bot;
+      default:
+        return Target.Neither;
+    }
+  }
+
   /**
    * canPerformModAction checks if a moderation action can be performed.
    * It sends the appropriate error message if not.
@@ -21,52 +59,47 @@ export default class Checks {
   static async canPerformModAction(
     message: Message,
     target: Member,
-    action: AllowedActions
+    action: AllowedActions,
+    silent: boolean = false
   ) {
     const Messages = rawMessages(action === 'BanMembers' ? 'ban' : 'kick');
 
     // Check if user invoking the command has enough permissions
-    if (!hasPermission(message.member!, message.channel!.server!, action)) {
-      message.channel?.sendMessage({
-        content: Messages.NoUserPermissions,
-      });
+    if (!this.userHasPermission(message, action)) {
+      if (!silent)
+        message.channel?.sendMessage({
+          content: Messages.NoUserPermissions,
+        });
 
       return false;
     }
 
     // Check if the bot has enough permissions
-    if (
-      !hasPermission(
-        message.channel!.server!.member!,
-        message.channel!.server!,
-        action
-      )
-    ) {
-      message.channel?.sendMessage({
-        content: Messages.NoBotPermissions,
-      });
+    if (!this.botHasPermission(message, action)) {
+      if (!silent)
+        message.channel?.sendMessage({
+          content: Messages.NoBotPermissions,
+        });
 
       return false;
     }
 
-    // Check if the user wants to ban themselves
-    if (target._id.user === message.author_id) {
-      message.channel?.sendMessage({
-        content: Messages.BanYourself,
-      });
-
-      return false;
+    // Check if the user wants to ban themselves or the bot
+    switch (this.checkTarget(message, target)) {
+      case Target.Author:
+        if (!silent)
+          message.channel?.sendMessage({
+            content: Messages.BanYourself,
+          });
+        return false;
+      case Target.Bot:
+        if (!silent)
+          message.channel?.sendMessage({
+            content: Messages.BanBot,
+          });
+        return false;
+      case Target.Neither:
+        return true;
     }
-
-    // Check if the user wants to ban the bot
-    if (target._id.user === message.client.user?._id) {
-      message.channel?.sendMessage({
-        content: Messages.BanBot,
-      });
-
-      return false;
-    }
-
-    return true;
   }
 }
